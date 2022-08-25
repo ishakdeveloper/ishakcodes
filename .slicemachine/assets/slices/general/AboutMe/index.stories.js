01cf7b11dec4d1146b8cfc3186a1af730f4730d9
ishakdeveloper <ishak.development@outlook.com>
2022-08-25 19:10:44 +0200

    init

---
+++ .slicemachine/assets/slices/general/AboutMe/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../../slices/general/AboutMe';
+
+export default {
+  title: 'slices/general/AboutMe'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Century","spans":[]}],"description":[{"type":"paragraph","text":"Ut officia est fugiat excepteur id nulla occaecat sint occaecat veniam. Qui magna qui excepteur. Ullamco dolore occaecat ad commodo quis veniam exercitation cupidatat id sunt in aliqua ut.","spans":[]}]},"slice_type":"about_me","id":"_Default"}} />
+_Default.storyName = ''
---
+++ .slicemachine/assets/slices/general/Hero/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../../slices/general/Hero';
+
+export default {
+  title: 'slices/general/Hero'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Difficulty","spans":[]}],"description":[{"type":"paragraph","text":"Exercitation reprehenderit nisi eu irure consequat reprehenderit minim in anim.","spans":[]}]},"slice_type":"hero","id":"_Default"}} />
+_Default.storyName = ''
